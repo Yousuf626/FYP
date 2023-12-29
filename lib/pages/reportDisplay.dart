655ee45c1cab25e9fde180f16e4d407fadf1781e
Mirza Musab Baig <faizanmusab54@gmail.com>
2023-12-30 01:31:00 +0500

    still better

--- lib/pages/reportDisplay.dart
+++ lib/pages/reportDisplay.dart
@@ -3,6 +3,7 @@ import 'package:aap_dev_project/models/report.dart';
 import 'package:flutter/material.dart';
 import 'bottomNavigationBar.dart';
 import 'appDrawer.dart';
+import 'package:flutter/material.dart';
 
 class ViewReport extends StatefulWidget {
   final UserReport? report;
@@ -16,38 +17,50 @@ class _ViewReportState extends State<ViewReport> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: const CustomDrawer(),
-        bottomNavigationBar: BaseMenuBar(),
-        body: Column(children: [
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height * 0.3,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(50.0),
-                bottomRight: Radius.circular(50.0),
-              ),
-              color: Color(0xFF01888B),
-            ),
-            child: Center(
-              child: Text(
-                widget.report!.type,
-                style: const TextStyle(
-                  fontSize: 36.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+      drawer: const CustomDrawer(),
+      bottomNavigationBar: BaseMenuBar(),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.3,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50.0),
+                    bottomRight: Radius.circular(50.0),
+                  ),
+                  color: Color(0xFF01888B),
+                ),
+                child: Center(
+                  child: Text(
+                    widget.report!.type,
+                    style: const TextStyle(
+                      fontSize: 36.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
-            ),
+              const SizedBox(height: 50.0),
+              Image.network(
+                widget.report!.image,
+                fit: BoxFit.contain,
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.5,
+              ),
+            ],
           ),
-          const SizedBox(height: 50.0),
-          Image.network(
-            widget.report!.image,
-            fit: BoxFit.contain,
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: MediaQuery.of(context).size.height * 0.5,
-          ),
-        ]));
+          Positioned(
+  top: 20,
+  left: 20,
+  child: BackButton(color: Colors.white),
+),
+        ],
+      ),
+    );
   }
 }
